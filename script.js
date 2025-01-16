5663277f866e07b044f09c78a90e768aef5a1e40
David Makaryan <115170622+makaryandavit@users.noreply.github.com>
2025-01-16 14:20:15 +0400

    solved 2.1.18

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
-let allExpense = [];
+// let allExpense = [];
 
-function calculateExpense() {
-  let expense = +prompt("Մուտքագրեք բենզինի ծախսը");
-  let mhGaloons = 0;
-  if (expense == -1) {
-    const allExpenseValue =
-      allExpense.reduce((a, b) => a + b, 0) / allExpense.length;
-    alert(allExpenseValue);
+// function calculateExpense() {
+//   let expense = +prompt("Մուտքագրեք բենզինի ծախսը");
+//   let mhGaloons = 0;
+//   if (expense == -1) {
+//     const allExpenseValue =
+//       allExpense.reduce((a, b) => a + b, 0) / allExpense.length;
+//     console.log(allExpenseValue);
+//     return;
+//   }
+//   let road = +prompt("Մուտքագրեք անցած ճանապարհը");
+
+//   mhGaloons = road / expense;
+//   allExpense.push(mhGaloons);
+//   console.log(mhGaloons);
+//   calculateExpense();
+// }
+
+// calculateExpense();
+
+// 2.1.18
+const allSales = [];
+const percent = 9;
+const share = 200;
+
+function countIncome() {
+  let income = 0;
+  let sale = +prompt("Введите объем продаж в долларах");
+  if (sale == -1) {
+    const allSalesValue = allSales.reduce((acc, value) => acc + value, 0);
+    console.log(`${Math.trunc(allSalesValue)}$`);
     return;
   }
-  let road = +prompt("Մուտքագրեք անցած ճանապարհը");
-
-  mhGaloons = road / expense;
-  allExpense.push(mhGaloons);
-  alert(mhGaloons);
-  calculateExpense();
+  income = (sale * percent) / 100 + share;
+  console.log(`${Math.trunc(income)}$`);
+  allSales.push(income);
+  countIncome();
 }
 
-calculateExpense();
+countIncome();
